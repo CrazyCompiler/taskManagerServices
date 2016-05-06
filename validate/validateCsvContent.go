9b32926534ac94258a4ded4c9dd98c080cf62ec0
VirajParab <parabveer01@gmail.com>
2016-05-06 11:28:57 +0530

    added nginx

--- validate/validateCsvContent.go
+++ validate/validateCsvContent.go
@@ -5,63 +5,57 @@ import (
 	"strconv"
 )
 
-type Validator interface  {
+type Validator interface {
 	hasError(eachRow []string) bool
-	GenerateErrorMessage(lineNo int,eachRow []string)string
+	GenerateErrorMessage(lineNo int, eachRow []string) string
 }
 
-
-type ValidNoOfColumn struct  {
+type ValidNoOfColumn struct {
 }
 
-func (c ValidNoOfColumn)hasError(eachRow []string) bool {
-	return len(eachRow)!=2
+func (c ValidNoOfColumn) hasError(eachRow []string) bool {
+	return len(eachRow) != 2
 }
 
-func (c ValidNoOfColumn)GenerateErrorMessage(lineNo int,eachRow []string)string  {
-	return "Line No "+strconv.Itoa(lineNo)+": expected 2 columns, but got "+strconv.Itoa(len(eachRow))+" columns\n"
+func (c ValidNoOfColumn) GenerateErrorMessage(lineNo int, eachRow []string) string {
+	return "Line No " + strconv.Itoa(lineNo) + ": expected 2 columns, but got " + strconv.Itoa(len(eachRow)) + " columns\n"
 }
 
-
-
-type TaskDescriptionChecker struct  {
+type TaskDescriptionChecker struct {
 }
 
-func (c TaskDescriptionChecker)hasError(eachRow []string)bool  {
+func (c TaskDescriptionChecker) hasError(eachRow []string) bool {
 	return eachRow[0] == ""
 }
 
-func (c TaskDescriptionChecker)GenerateErrorMessage(lineNo int,eachRow []string)string  {
-	return "Line No "+strconv.Itoa(lineNo)+",Column No 1:"+" Task DescripTion Can't be empty\n"
+func (c TaskDescriptionChecker)GenerateErrorMessage(lineNo int, eachRow []string) string {
+	return "Line No " + strconv.Itoa(lineNo) + ",Column No 1:" + " Task DescripTion Can't be empty\n"
 }
 
-
-
-type PriorityChecker struct  {
+type PriorityChecker struct {
 }
 
-func (c PriorityChecker)hasError(eachRow []string) bool {
+func (c PriorityChecker) hasError(eachRow []string) bool {
 	return eachRow[1] != "High" && eachRow[1] != "Medium" && eachRow[1] != "Low"
 }
 
-func (c PriorityChecker)GenerateErrorMessage(lineNo int,eachRow []string)string  {
-	return "Line No "+strconv.Itoa(lineNo)+",Column No 2:"+" expected priority is High or Medium or Low, but got "+eachRow[1]+"\n"
+func (c PriorityChecker) GenerateErrorMessage(lineNo int, eachRow []string) string {
+	return "Line No " + strconv.Itoa(lineNo) + ",Column No 2:" + " expected priority is High or Medium or Low, but got " + eachRow[1] + "\n"
 }
-
 
 func ValidateAllEntry(allEntry [][]string, allValidators []Validator) error {
 	errMsg := ""
 	count := 1
-	for _,eachEntry := range allEntry {
-		for _,eachValidator :=range allValidators{
-			if eachValidator.hasError(eachEntry){
-				errMsg +=eachValidator.GenerateErrorMessage(count,eachEntry)
+	for _, eachEntry := range allEntry {
+		for _, eachValidator := range allValidators {
+			if eachValidator.hasError(eachEntry) {
+				errMsg += eachValidator.GenerateErrorMessage(count, eachEntry)
 			}
 		}
 		count++
 	}
-	if errMsg!="" {
-		return errors.New("errors in the following line : \n"+errMsg)
+	if errMsg != "" {
+		return errors.New("errors in the following line : \n" + errMsg)
 	}
 	return nil
 }
